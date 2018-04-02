7793e3552246704822ade711ac0d53f8795db3f8
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2018-04-01 21:30:12 -0300

    add parsing of parameters

--- src/main/java/ar/edu/itba/ss/g6/tp/TP3.java
+++ src/main/java/ar/edu/itba/ss/g6/tp/TP3.java
@@ -1,8 +1,23 @@
 package ar.edu.itba.ss.g6.tp;
 
+import ar.edu.itba.ss.g6.tp.tp3.CommandLineOptions;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
 public class TP3 {
 
     public static int main(String ...args) {
+
+
+        CommandLineOptions values = new CommandLineOptions(args);
+        CmdLineParser parser = new CmdLineParser(values);
+
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.exit(1);
+        }
+
         return 0;
     }
 }
