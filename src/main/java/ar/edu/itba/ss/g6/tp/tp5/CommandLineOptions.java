e12aa9425a8c214601d1b2764ee732253a338184
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2018-05-15 04:11:54 -0300

    feat(TP5): double the time resolution

--- src/main/java/ar/edu/itba/ss/g6/tp/tp5/CommandLineOptions.java
+++ src/main/java/ar/edu/itba/ss/g6/tp/tp5/CommandLineOptions.java
@@ -65,7 +65,7 @@ public class CommandLineOptions {
 
     @Option(name = "-ts", aliases = { "--time-step" }, required = false,
      usage = "time step for simulation")
-    private double timeStep = .00002;
+    private double timeStep = .00001;
 
     @Option(name = "-f", aliases = { "--fps" }, required = false,
      usage = "frames per second")
